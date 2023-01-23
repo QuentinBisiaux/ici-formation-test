169da231fec2071c9de62c0d2ca1b0d8128d5692
Quentin <quentin.bisiaux@gmail.com>
2023-01-23 14:40:38 +0100

    basic design of formation crud

--- src/Form/TrainingType.php
+++ src/Form/TrainingType.php
@@ -4,16 +4,27 @@ namespace App\Form;
 
 use App\Entity\Training;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class TrainingType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('name')
-        ;
+        $builder->add(
+            'name',
+            TextType::class,
+            [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Renseigner un Titre pour la formation',
+                    ])
+                ]
+            ]
+        );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
