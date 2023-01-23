eae5cbad5acfd9b194f83d565377b50e51e9fb3c
Quentin <quentin.bisiaux@gmail.com>
2023-01-23 14:11:28 +0100

    add required to password

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -34,8 +34,7 @@ class RegistrationFormType extends AbstractType
                 ]
             ])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+                'required' => true,
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
